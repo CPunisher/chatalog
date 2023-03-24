242688bf4260c9ffb21a6f5183c5ca0c523d4107
CPunisher <1343316114@qq.com>
2023-03-24 19:53:22 +0800

    feat: report page init

--- report-page/index.tsx
+++ report-page/index.tsx
@@ -1,9 +1,57 @@
-import { render } from "preact";
+import { createContext, render } from "preact";
+import { useContext, useState } from "preact/hooks";
 import { GroupedDatalogFiles } from "../chatalog-cli/interface";
 import "./index.css";
 
+interface ReportContextProps {
+  data: GroupedDatalogFiles[];
+  current: GroupedDatalogFiles;
+  setCurrent: (newCurrent: GroupedDatalogFiles) => void;
+}
+
+const ReportContext = createContext<ReportContextProps | null>(null);
+
+const Sidebar = () => {
+  const { data = [], current, setCurrent } = useContext(ReportContext) || {};
+  return (
+    <div class="dark hidden bg-gray-900 p-2 space-y-1 md:fixed md:inset-y-0 md:flex md:w-[260px] md:flex-col">
+      <div class="flex flex-col gap-2 text-gray-100 text-sm">
+        {data.map((group) => (
+          <a class="flex py-3 px-3 items-center gap-3 relative rounded-md hover:bg-[#2A2B32] cursor-pointer break-all hover:pr-4 group">
+            <div class="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative">
+              {group.rules[0].name}
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Conversation = () => {
+  return <div>Conversation</div>;
+};
+
 function renderReports(parentNode: Element, data: GroupedDatalogFiles[]) {
-  render(<h1>Hello World23445</h1>, parentNode);
+  const [current, setCurrent] = useState(data[0]);
+  const Page = (
+    <div class="overflow-hidden w-full h-full">
+      <Sidebar />
+      <Conversation />
+    </div>
+  );
+  render(
+    <ReportContext.Provider
+      value={{
+        data,
+        current,
+        setCurrent,
+      }}
+    >
+      {Page}
+    </ReportContext.Provider>,
+    parentNode
+  );
 }
 
 export default renderReports;
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,30 +1,30 @@
-import { defineConfig } from 'rollup';
-import typescript from '@rollup/plugin-typescript';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import tailwindcss from 'tailwindcss';
-import postcss from 'rollup-plugin-postcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import tailwindcss from "tailwindcss";
+import postcss from "rollup-plugin-postcss";
+import autoprefixer from "autoprefixer";
 import tailwindConfig from "./tailwind.config.js";
 
 export default defineConfig({
-  input: 'report-page/index.tsx',
+  input: "report-page/index.tsx",
   output: {
-    format: 'iife',
-    file: 'dist/report-page/index.js',
-    name: 'renderReports',
-    exports: 'named',
+    format: "iife",
+    file: "dist/report-page/index.js",
+    name: "renderReports",
+    exports: "named",
   },
   plugins: [
     typescript({
-      module: 'esnext',
+      module: "esnext",
       compilerOptions: {
-        outDir: 'dist/report-page',
-      }
+        outDir: "dist/report-page",
+      },
     }),
     nodeResolve(),
     postcss({
-      extensions: ['.css'],
-      plugins: [autoprefixer(), tailwindcss(tailwindConfig)]
+      extensions: [".css"],
+      plugins: [autoprefixer(), tailwindcss(tailwindConfig)],
     }),
   ],
 });
