154909fc4805e0b83a8d467fcbd557660e7b2e6f
CPunisher <1343316114@qq.com>
2023-04-04 23:11:13 +0800

    feat: origin file

---
+++ report-page/components/MessageItem.tsx
@@ -0,0 +1,29 @@
+import classNames from "classnames";
+import { FunctionalComponent, VNode } from "preact";
+
+interface MessageItemProps {
+  className?: string;
+  prompt?: VNode;
+}
+
+const MessageItem: FunctionalComponent<MessageItemProps> = ({
+  className,
+  prompt,
+  children,
+}) => {
+  return (
+    <div
+      class={classNames(
+        "w-full text-gray-800 border-b border-black/10",
+        className
+      )}
+    >
+      <div class="flex gap-4 text-base whitespace-pre-wrap m-auto p-4  md:gap-6 md:max-w-2xl md:py-6 lg:max-w-2xl xl:max-w-3xl">
+        {prompt && <div>{prompt}</div>}
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default MessageItem;
--- report-page/layout/File.tsx
+++ report-page/layout/File.tsx
@@ -1,7 +1,42 @@
+import classNames from "classnames";
 import { FunctionalComponent } from "preact";
+import { useContext } from "preact/hooks";
+import MessageItem from "../components/MessageItem";
+import ReportContext from "../context";
 
 const File: FunctionalComponent = () => {
-  return <div>File</div>;
+  const { current } = useContext(ReportContext) || {};
+
+  if (!current) {
+    return null;
+  }
+
+  const list = [
+    ...current.rules.map((rule, i) => [
+      `Rule[${i + 1}]: ${rule.name}`,
+      rule.content,
+    ]),
+    ...current.facts.map((fact, i) => [
+      `Fact[${i + 1}]: ${fact.name}`,
+      fact.content,
+    ]),
+    ...current.expected.map((expected, i) => [
+      `Expected[${i + 1}]: ${expected.name}`,
+      expected.content,
+    ]),
+  ];
+
+  return (
+    <div className="flex h-full flex-col overflow-auto">
+      {list.map(([prompt, content], index) => (
+        <MessageItem className={classNames({ "bg-gray-50": index % 2 === 1 })}>
+          <div className="py-1 font-bold">{prompt}</div>
+          {content}
+        </MessageItem>
+      ))}
+      <div class="w-full h-32 md:h-48" />
+    </div>
+  );
 };
 
 export default File;
