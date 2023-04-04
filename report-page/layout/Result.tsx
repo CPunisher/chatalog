022ebe0d4c451859e715c91d61cc095d1447abf3
CPunisher <1343316114@qq.com>
2023-04-05 01:25:53 +0800

    feat: basic report result

--- report-page/layout/Result.tsx
+++ report-page/layout/Result.tsx
@@ -1,7 +1,21 @@
 import { FunctionalComponent } from "preact";
+import { useContext } from "preact/hooks";
+import MessageItem from "../components/MessageItem";
+import ReportContext from "../context";
 
 const Result: FunctionalComponent = () => {
-  return <div>Result</div>;
+  const { current } = useContext(ReportContext) || {};
+
+  if (!current) {
+    return null;
+  }
+
+  return (
+    <div className="flex h-full flex-col overflow-auto">
+      <MessageItem>{current.testResult?.actual}</MessageItem>
+      <div class="w-full h-32 md:h-48" />
+    </div>
+  );
 };
 
 export default Result;
